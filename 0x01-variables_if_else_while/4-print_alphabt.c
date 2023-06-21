01602372a73370a93a31f8d79c3e4b89662b8d50
Hartlief <lesleyannehartlief@gmail.com>
2023-06-21 05:55:22 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}	
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}	
